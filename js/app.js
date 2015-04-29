377c51ec24b3ca97fee7fd30dac63129a1ca8368
Nethra Balasundaram <nethra.pd41@gmail.com>
2015-04-29 11:13:52 -0400

    Adding weather news
    integrating weather news api

--- js/app.js
+++ js/app.js
@@ -1,18 +1,21 @@
-
+// Module
 var app = angular.module('home', []);
-
+//weather controller
 function weatherController($scope, $http) {
+	// default search term on page load
 	$scope.searchTerm = 'boston';
 	$scope.date = [];
 	var day = new Date();
 	var nextDate = day;
 	$scope.date.push(day);
+	// list of dates from current date +6 
 		for(i=1; i<7; i++) 
 		{
 			var nextDay = new Date(nextDate);
 			var nextDate = nextDay.setDate(day.getDate()+i);
 			$scope.date.push(nextDate);
 		}
+	// gets data from API
 	$scope.getData = function(indx) {
 	var a = indx;
 	term = $scope.searchTerm;
@@ -35,5 +38,15 @@ function weatherController($scope, $http) {
       });
 	}
 }
+// News Controller
+function newsController($scope, $http) {
+
+	$scope.getNews = function() {
+	$http.jsonp('http://api.usatoday.com/open/articles?section=weather&api_key=38mx6ck4dufzcw8z488hygjk&encoding=json&jsoncallbackmethod=JSON_CALLBACK').success(function(data) {
+	  
+        $scope.stories = data.stories;
+	   console.log(data.stories[0].description);
+      });
 	
-	
+	}
+}
